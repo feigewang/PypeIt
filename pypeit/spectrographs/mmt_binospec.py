fbd2cd065df154045d6c122848ebf947d933fa8c
gphil1 <gphil@umich.edu>
2025-05-18 18:51:36 -0400

    Turn off embed

--- pypeit/spectrographs/mmt_binospec.py
+++ pypeit/spectrographs/mmt_binospec.py
@@ -260,10 +260,9 @@ class MMTBINOSPECSpectrograph(spectrograph.Spectrograph):
             par['calibrations']['wavelengths']['reid_arxiv'] = 'mmt_binospec_1000.fits'
 
         headarr = self.get_headarr(scifile)
-        debug_param = self.get_meta_value(headarr, 'MASK')
         # Turn on the use of mask design
-        import IPython;
-        IPython.embed()
+        #import IPython;
+        #IPython.embed()
         if ('Longslit' not in self.get_meta_value(headarr, 'target')):
             # TODO -- Move this parameter into SlitMaskPar??
             par['calibrations']['slitedges']['use_maskdesign'] = True
