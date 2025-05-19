cef2254fe9ce14a5b600a910bd50c1e01b6b2c8c
gphil1 <gphil@umich.edu>
2025-05-19 14:27:08 -0400

    Comment out embed; ready to run reduction

--- pypeit/spectrographs/mmt_binospec.py
+++ pypeit/spectrographs/mmt_binospec.py
@@ -260,11 +260,11 @@ class MMTBINOSPECSpectrograph(spectrograph.Spectrograph):
             par['calibrations']['wavelengths']['reid_arxiv'] = 'mmt_binospec_1000.fits'
 
         headarr = self.get_headarr(scifile)
-        debug_param = self.get_meta_value(headarr, 'decker')
 
         # Turn on the use of mask design
-        import IPython;
-        IPython.embed()
+
+        #import IPython;
+        #IPython.embed()
         if ('Longslit' not in self.get_meta_value(headarr, 'decker')):
             # TODO -- Move this parameter into SlitMaskPar??
             par['calibrations']['slitedges']['use_maskdesign'] = True
