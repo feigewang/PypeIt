891cdf60c4bec3d65b1ae99daafac5aba73de3b2
gphil1 <gphil@umich.edu>
2025-06-01 17:39:55 -0400

    Debug in flexure.py

--- pypeit/core/flexure.py
+++ pypeit/core/flexure.py
@@ -653,6 +653,11 @@ def spec_flex_shift_local(slits, slitord, specobjs, islit, sky_file, empty_flex_
     # Append flex_dict, which will be an empty dictionary if the flexure failed for the whole slit
     flex_list.append(flex_dict.copy())
 
+    # Debug
+    # print("Entering embed in spec_flex_shift_local()")
+    # from IPython import embed
+    # embed()
+
     return flex_list
 
 
@@ -811,23 +816,25 @@ def spec_flexure_slit(slits, slitord, slit_bpm, sky_file, method="boxcar", speco
                     i_slitord = slitord[sidx]
                     indx = specobjs.slitorder_indices(i_slitord)
 
+                    # Start fresh
+                    flex_dict = copy.deepcopy(empty_flex_dict)
+
+                    # Fill with repeated median values
                     for i in range(len(specobjs[indx])):
-                        # Reset the dict
-                        flex_dict = copy.deepcopy(empty_flex_dict)
-                        # Update dict
                         for key in keys_to_update[:-1]:
                             flex_dict[key].append(fdict[key][0])
-                        # Interpolate
+                        # Interpolate sky
                         sky_wave_new = flexure_interp(fdict['shift'][0], specobjs[indx][i].BOX_WAVE)
                         flex_dict['sky_spec'].append(
                             xspectrum1d.XSpectrum1D.from_tuple((sky_wave_new, specobjs[indx][i].BOX_COUNTS_SKY)))
-                        # insert flex_dict in flex_list at the location of the slit that failed the calculation
-                        flex_list[sidx] = flex_dict
+
+                    # Now update flex_list once
+                    flex_list[sidx] = flex_dict
 
             #Debug
-            print("Entering embed in spec_flex_shift()")
-            from IPython import embed
-            embed()
+            #print("Entering embed in spec_flex_shift()")
+            #from IPython import embed
+            #embed()
 
 
 
