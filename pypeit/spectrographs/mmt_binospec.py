434e4b4cccd33cd8929c55853f9e2c3e79dd7885
gphil1 <gphil@umich.edu>
2025-07-07 16:06:47 -0500

    Adjust coadd2d offset parameter

--- pypeit/spectrographs/mmt_binospec.py
+++ pypeit/spectrographs/mmt_binospec.py
@@ -302,7 +302,6 @@ class MMTBINOSPECSpectrograph(spectrograph.Spectrograph):
 
             if decker == 'J0226p0302_mask1_830':
                 par['coadd2d']['offsets'] = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,0., 0., 0., 0., 0., 0., 0., 0., 0.]
-
             else:
                 par['coadd2d']['offsets'] = 'maskdef_offsets'
 
